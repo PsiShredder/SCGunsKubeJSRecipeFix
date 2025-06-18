40c866cd2fba1a167518742afc26d085c238f074
Joe Weilert <140188018+PsiShredder@users.noreply.github.com>
2025-06-18 15:23:59 -0600

    Update SCGunsBuckshotRecipeFix.js

--- SCGunsBuckshotRecipeFix.js
+++ SCGunsBuckshotRecipeFix.js
@@ -1,12 +1,13 @@
 //Place this file into the server_scripts/ folder after installing KubeJS
 
 ServerEvents.recipes( event => {
-  event.remove({ id: 'scguns:buckshot' })
-  event.shapeless(
+event.shapeless(
     Item.of('scguns:buckshot', 6),
       [
         'forge:gunpowder',
         '4x scguns:stan_bullet_tips'        
       ]
-    ).id('scguns:buckshot_manual_only')
+    ).id('kubejs:buckshot_manual_only')
+  event.remove({id: 'scguns:buckshot' })
+  })
 })
