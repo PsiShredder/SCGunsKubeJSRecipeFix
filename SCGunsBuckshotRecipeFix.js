6e0f61f893195fb5c407025482ed46f43eefcacd
Joe Weilert <140188018+PsiShredder@users.noreply.github.com>
2025-06-18 16:25:51 -0600

    Finalize SCGunsBuckshotRecipeFix.js
    1.0 version of the script, replaces Scorched Guns 2 buckshot shapeless recipe with an identical recipe with "_manual_only" and the end of the recipe id. This prevents create mixers from automating the shapeless instead of using the mixing recipe.

--- SCGunsBuckshotRecipeFix.js
+++ SCGunsBuckshotRecipeFix.js
@@ -1,12 +1,14 @@
 //Place this file into the server_scripts/ folder after installing KubeJS
 
 ServerEvents.recipes( event => {
-event.shapeless(
-    Item.of('scguns:buckshot', 6),
-      [
-        'forge:gunpowder',
-        '4x scguns:stan_bullet_tips'        
-      ]
-    ).id('kubejs:buckshot_manual_only')
-  event.remove({id: 'scguns:buckshot' })
+	event.shapeless(
+		Item.of('scguns:buckshot', 6),
+      			[
+        		{"tag": "forge:gunpowder"},
+        		{"tag": "scguns:stan_bullet_tips"},
+    			{"tag": "scguns:stan_bullet_tips"},
+    			{"tag": "scguns:stan_bullet_tips"},
+    			{"tag": "scguns:stan_bullet_tips"}]
+	).id('kubejs:buckshot_manual_only')
+	event.remove({id: 'scguns:buckshot'})
 })
