18aceb6f2e5d005a1b93b1a6d20cf21860ba7596
Joe Weilert <140188018+PsiShredder@users.noreply.github.com>
2025-06-18 16:44:02 -0600

    Update SCGunsBuckshotRecipeFix.js
    Final final version

--- SCGunsBuckshotRecipeFix.js
+++ SCGunsBuckshotRecipeFix.js
@@ -3,12 +3,11 @@
 ServerEvents.recipes( event => {
 	event.shapeless(
 		Item.of('scguns:buckshot', 6),
-      			[
-        		{"tag": "forge:gunpowder"},
+      			[{"tag": "forge:gunpowder"},
         		{"tag": "scguns:stan_bullet_tips"},
     			{"tag": "scguns:stan_bullet_tips"},
     			{"tag": "scguns:stan_bullet_tips"},
     			{"tag": "scguns:stan_bullet_tips"}]
-	).id('kubejs:buckshot_manual_only')
+	).id('scguns:buckshot_manual_only')
 	event.remove({id: 'scguns:buckshot'})
 })
